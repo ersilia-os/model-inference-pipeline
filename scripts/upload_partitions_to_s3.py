e034982e39f95e350f39e1cbb8885c2e640e551f
kabilank6 <52188518+kabilank6@users.noreply.github.com>
2024-06-17 20:05:04 +1200

    refactor AWS S3 upload functionality from predict.yml into a Python script

---
+++ scripts/upload_partitions_to_s3.py
@@ -0,0 +1,12 @@
+import os
+
+import subprocess
+
+numerator = int(os.environ.get('numerator'))
+input_sha = os.environ.get('SHA')
+
+output_file = "../{}_{}.csv".format(input_sha, numerator)
+
+s3_destination = "s3://precalculations-bucket/out/{}/{}/{}_{}.csv".format(
+    os.environ.get('model-id'), input_sha, input_sha, numerator)
+subprocess.run(["aws", "s3", "cp", output_file, s3_destination])
