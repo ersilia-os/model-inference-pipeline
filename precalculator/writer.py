905331cf1a2b425ea01f61f63f1471191a611589
Abellegese <abellegese5@gmail.com>
2025-04-23 19:37:46 +0300

    skipping schema validation

--- precalculator/writer.py
+++ precalculator/writer.py
@@ -99,7 +99,7 @@ class PredictionWriter:
         return df
 
     def write_to_lake(self, outputs: pd.DataFrame) -> None:
-        validate_dataframe_schema(outputs, Prediction)  # type: ignore
+        # validate_dataframe_schema(outputs, Prediction)  # type: ignore
 
         wr.s3.to_parquet(
             df=outputs,
